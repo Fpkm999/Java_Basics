bd5166cd94cc4e0159e66f07d66e364b049b509b
Fpkm999 <144207339+Fpkm999@users.noreply.github.com>
2023-11-28 22:11:48 +0900

    2023 - 11 - 28 TIL

--- section12/bluetooth/BluetoothMIC2.java
+++ section12/bluetooth/BluetoothMIC2.java
@@ -1,5 +1,7 @@
 package section12.bluetooth;
-//자식 인터페이스O . 구현클래스가 아님. 그래서 재정의 안해도됨.
+// 자식 인터페이스
+// 구현클래스가 아니다.
+// 자식 인터페이스는 구현 클래스가 아니기 때문에 재정의 // 안해도됨.
 public interface BluetoothMIC2 extends MicroPhone, Speaker {
 	public abstract void connect();
 	
