40494cb6bcf0479420469b3db81348846a84b110
Fpkm999 <Fpkm999@proton.me>
2023-11-29 17:03:14 +0900

    2023-11-29 TIL

--- section16/BlackJack/CardDeck.java
+++ section16/BlackJack/CardDeck.java
@@ -10,6 +10,7 @@ package section16.BlackJack;
 
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CardDeck {
@@ -17,35 +18,40 @@ public class CardDeck {
 
   private static final String[] PATTERNS = {"spade","heart","diamond","club"};
   private static final int CARD_COUNT = 13; // 카드 총 숫자
-  public CardDeck(){ // 생성자
-    cards = new ArrayList<>();
+  public CardDeck() { // 생성자
+    cards = this.generateCards();
+  }
+  private List<Card> generateCards() {  // 52개의 카드를 생성
+    List<Card> cards = new LinkedList<>();
 
-    for(String pattern : PATTERNS){// 4번
-      for(int i =1; i<=CARD_COUNT; i++){ // 13번 = 총 52번 반복함
+    for (String pattern : PATTERNS) {// 4번
+      for (int i = 1; i <= CARD_COUNT; i++) { // 13번 = 총 52번 반복함
         Card card = new Card();
-        String denomination;
+        String denomination = numberToDenomination(i);
 
-        if(i == 1){
-          denomination = "A";
-        } else if (i == 11){
-          denomination = "J";
-        } else if (i == 12){
-          denomination = "Q";
-        } else if (i == 13){
-          denomination = "K";
-        } else {
-          denomination = String.valueOf(i);
-        }
         card.setDenomination(denomination);
         card.setPattern(pattern);
         cards.add(card);
-        System.out.println("카드덱 소환"+cards.size());
-        System.out.println("카드덱 소환"+cards.toString());
+        System.out.println(".카드덱 소환" + cards.size());
+        System.out.println(".카드덱 소환" + cards.toString());
       }
     }
-
+      return cards;
   }
 
+
+  private String numberToDenomination(int number){
+    if(number == 1){
+      return  "A";
+    } else if (number == 11){
+      return "J";
+    } else if (number == 12){
+      return "Q";
+    } else if (number == 13){
+      return "K";
+    }
+      return String.valueOf(number);
+    } // 숫자를 끗수로
 
   public Card getCard(){
     return null;  //일단 null
